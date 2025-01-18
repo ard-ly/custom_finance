9f929bc89c79f7e49f4353ec753183569dbed569
omarjaber <omar.ja93@gmail.com>
2025-01-18 16:05:38 +0200

    add workspace

--- custom_finance/custom_finance/doctype/top_up_transactions/top_up_transactions.js
+++ custom_finance/custom_finance/doctype/top_up_transactions/top_up_transactions.js
@@ -1,8 +1,24 @@
 // Copyright (c) 2024, custom_finance and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Top-up Transactions", {
-// 	refresh(frm) {
+frappe.ui.form.on("Top-up Transactions", {
+	operation_type: function(frm) {
+        if(frm.doc.operation_type){
+			frappe.model.with_doc("Operation Type", frm.doc.operation_type, function() {
+			    var tabletransfer= frappe.model.get_doc("Operation Type", frm.doc.operation_type)
+			    frm.clear_table("transactions_account");
 
-// 	},
-// });
+		        // Add first row
+                var row1 = frm.add_child("transactions_account");
+                row1.account = tabletransfer.operation_debit_account;
+
+                // Add second row
+                var row2 = frm.add_child("transactions_account");
+                row2.account = tabletransfer.operation_credit_account;
+                
+		        frm.refresh_field("transactions_account");
+
+			})
+        }
+    }
+});
